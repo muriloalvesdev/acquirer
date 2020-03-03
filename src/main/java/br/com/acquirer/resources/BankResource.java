09a8312c6e4f37228e03560d617d11d8cea29762
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-03 13:59:25 -0300

    nova versao do bank

--- src/main/java/br/com/acquirer/resources/BankResource.java
+++ src/main/java/br/com/acquirer/resources/BankResource.java
@@ -4,38 +4,31 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BankResource {
 
-  @JsonProperty("name")
-  private String name;
-
-  @JsonProperty("agency")
-  private String agency;
-
-  @JsonProperty("account")
-  private String account;
-
   @JsonProperty("amount_sale")
   private String amountSale;
 
-  public String getName() {
-    return name;
-  }
-
-  public String getAgency() {
-    return agency;
-  }
-
-  public String getAccount() {
-    return account;
-  }
+  @JsonProperty("card")
+  private CardsDataTransferObject card;
 
   public String getAmountSale() {
     return amountSale;
   }
 
+  public void setAmountSale(String amountSale) {
+    this.amountSale = amountSale;
+  }
+
+  public CardsDataTransferObject getCard() {
+    return card;
+  }
+
+  public void setCard(CardsDataTransferObject card) {
+    this.card = card;
+  }
+
   @Override
   public String toString() {
-    return "BankDataTransferObject [name=" + name + ", agency=" + agency + ", account=" + account
-        + ", amountSale=" + amountSale + "]";
+    return "BankResource [amountSale=" + amountSale + ", card=" + card + "]";
   }
 
 }
