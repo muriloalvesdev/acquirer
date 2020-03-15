41d1e43068c5d49f86708ecc7d750c7b26fc1368
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 16:06:01 -0300

    sem try/catch neste caso

--- src/main/java/br/com/acquirer/service/establishment/EstablishmentService.java
+++ src/main/java/br/com/acquirer/service/establishment/EstablishmentService.java
@@ -2,7 +2,6 @@ package br.com.acquirer.service.establishment;
 
 import java.security.InvalidParameterException;
 import java.util.Optional;
-import org.jboss.logging.Logger;
 import org.springframework.stereotype.Service;
 import br.com.acquirer.convert.EstablishmentConvertDTO;
 import br.com.acquirer.domain.model.Establishment;
@@ -12,8 +11,6 @@ import br.com.acquirer.dto.EstablishmentDataTransferObject;
 @Service
 public class EstablishmentService {
 
-  private static final Logger LOG = Logger.getLogger(EstablishmentService.class);
-
   private EstablishmentRepository establishmentRepository;
 
   public EstablishmentService(EstablishmentRepository establishmentRepository) {
@@ -21,15 +18,12 @@ public class EstablishmentService {
   }
 
   public EstablishmentDataTransferObject findByMerchantCode(String merchantCode) {
+
     Optional<Establishment> establishmentOptional =
         establishmentRepository.findByMerchantCode(Long.parseLong(merchantCode));
-
-    try {
-      checkEstablishmentExist(merchantCode, establishmentOptional);
-    } catch (Exception e) {
-      LOG.error(e.getMessage(), e);
-    }
+    checkEstablishmentExist(merchantCode, establishmentOptional);
     Establishment establishment = establishmentOptional.get();
+
     return EstablishmentConvertDTO.converToDTO(establishment);
   }
 
