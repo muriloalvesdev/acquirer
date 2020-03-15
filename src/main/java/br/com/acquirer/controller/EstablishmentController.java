9834be937b497d9af484058a2e643e30d9e2092d
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 00:18:47 -0300

    servico, controller e imagem swagger - para exibir no readme do projeto

---
+++ src/main/java/br/com/acquirer/controller/EstablishmentController.java
@@ -0,0 +1,23 @@
+package br.com.acquirer.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.acquirer.dto.EstablishmentDataTransferObject;
+import br.com.acquirer.service.establishment.EstablishmentService;
+
+@RestController
+@RequestMapping("/api/establishment")
+public class EstablishmentController {
+
+  private EstablishmentService service;
+
+  @GetMapping("find/{merchantcode}")
+  public ResponseEntity<EstablishmentDataTransferObject> findByMerchantCode(
+      @PathVariable(name = "merchantcode", required = true) String merchantCode) {
+    return ResponseEntity.ok(service.findByMerchantCode(merchantCode));
+  }
+
+}
