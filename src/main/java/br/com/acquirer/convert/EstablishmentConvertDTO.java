0c625f465f89242b4529c6153685d165ddb56f89
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 01:24:32 -0300

    apenas convert para DTO é necessário por equanto

--- src/main/java/br/com/acquirer/convert/EstablishmentConvertDTO.java
+++ src/main/java/br/com/acquirer/convert/EstablishmentConvertDTO.java
@@ -1,23 +1,13 @@
 package br.com.acquirer.convert;
 
-import br.com.acquirer.domain.model.Acquirer;
 import br.com.acquirer.domain.model.Establishment;
 import br.com.acquirer.dto.EstablishmentDataTransferObject;
 
 public final class EstablishmentConvertDTO {
 
-  public static Establishment convert(EstablishmentDataTransferObject establishmentDTO,
-      Acquirer acquirer) {
-
-    return new Establishment(establishmentDTO.getName(),
-        Long.parseLong(establishmentDTO.getMerchantCode()), acquirer,
-        Double.parseDouble(establishmentDTO.getMDR()));
-  }
-
   public static EstablishmentDataTransferObject converToDTO(Establishment establishment) {
     return new EstablishmentDataTransferObject(establishment.getName(),
-        establishment.getMerchantCode().toString(),
-        establishment.getAcquirer().getAcquirerName().name(), establishment.getMDR().toString());
+        establishment.getMerchantCode().toString());
   }
 
 }
