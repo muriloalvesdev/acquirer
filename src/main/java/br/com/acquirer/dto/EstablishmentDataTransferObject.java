a7610229a50121e2e55edcfd612562b853fc9f96
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 01:25:10 -0300

    DTO do establishment deve retornar apenas o nome do establishment and merchantcode

--- src/main/java/br/com/acquirer/dto/EstablishmentDataTransferObject.java
+++ src/main/java/br/com/acquirer/dto/EstablishmentDataTransferObject.java
@@ -10,18 +10,9 @@ public class EstablishmentDataTransferObject {
   @JsonProperty("merchant_code")
   private String merchantCode;
 
-  @JsonProperty("acquirer_name")
-  private String acquirerName;
-
-  @JsonProperty("merchant_discount_rate")
-  private String MDR;
-
-  public EstablishmentDataTransferObject(String name, String merchantCode, String acquirerName,
-      String MDR) {
+  public EstablishmentDataTransferObject(String name, String merchantCode) {
     this.name = name;
     this.merchantCode = merchantCode;
-    this.acquirerName = acquirerName;
-    this.MDR = MDR;
   }
 
   public String getName() {
@@ -32,18 +23,9 @@ public class EstablishmentDataTransferObject {
     return merchantCode;
   }
 
-  public String getAcquirerName() {
-    return acquirerName;
-  }
-
-  public String getMDR() {
-    return MDR;
-  }
-
   @Override
   public String toString() {
-    return "EstablishmentDataTransferObject [name=" + name + ", merchantCode=" + merchantCode
-        + ", acquirerName=" + acquirerName + ", MDR=" + MDR + "]";
+    return "EstablishmentDataTransferObject [name=" + name + ", merchantCode=" + merchantCode + "]";
   }
 
 }
