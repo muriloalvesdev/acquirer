7bad32da13a9963d8f9656c6885d07094b54d395
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 00:18:05 -0300

    mais um convert para o estabelecimento

--- src/main/java/br/com/acquirer/convert/EstablishmentConvertDTO.java
+++ src/main/java/br/com/acquirer/convert/EstablishmentConvertDTO.java
@@ -2,7 +2,7 @@ package br.com.acquirer.convert;
 
 import br.com.acquirer.domain.model.Acquirer;
 import br.com.acquirer.domain.model.Establishment;
-import br.com.acquirer.resources.EstablishmentDataTransferObject;
+import br.com.acquirer.dto.EstablishmentDataTransferObject;
 
 public final class EstablishmentConvertDTO {
 
@@ -12,6 +12,12 @@ public final class EstablishmentConvertDTO {
     return new Establishment(establishmentDTO.getName(),
         Long.parseLong(establishmentDTO.getMerchantCode()), acquirer,
         Double.parseDouble(establishmentDTO.getMDR()));
+  }
+
+  public static EstablishmentDataTransferObject converToDTO(Establishment establishment) {
+    return new EstablishmentDataTransferObject(establishment.getName(),
+        establishment.getMerchantCode().toString(),
+        establishment.getAcquirer().getAcquirerName().name(), establishment.getMDR().toString());
   }
 
 }
