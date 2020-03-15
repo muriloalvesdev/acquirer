39189c950190ca5657ff6fdf104fd9f445b88582
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-15 16:00:49 -0300

    SaleController deve validar se os campos do json estão todos preenchidos

--- src/main/java/br/com/acquirer/controller/SaleController.java
+++ src/main/java/br/com/acquirer/controller/SaleController.java
@@ -2,6 +2,7 @@ package br.com.acquirer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class SaleController {
   private AcquirerService acquirerService;
 
   @PostMapping("create")
-  public ResponseEntity<Void> requestHolder(@RequestBody RequestResource request) {
+  public ResponseEntity<Void> requestHolder(@Validated @RequestBody RequestResource request) {
     acquirerService.sendRequestToModules(request);
     return ResponseEntity.ok().build();
   }
